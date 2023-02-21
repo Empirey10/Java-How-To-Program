f4ddb07ad4e8f706d05cf85264c3bf39e92ea3a2
Empirey10 <genzyy04@gmail.com>
2023-02-20 21:32:15 -0800

    add file program

---
+++ Welcome1.java
@@ -0,0 +1,11 @@
+ // Fig. 2.1: Welcome.java
+ // Text-printing program.
+
+ public class Welcome1
+ {
+   // main method begins execution of Java application
+   public static void main(String[] args )
+   {
+     System.out.println( "Welcome to Java Programming!" );
+   } // end method main
+} // end class welcome1
