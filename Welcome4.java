0a16e71b366c0da1f3c02271afc81df30cb2a4f9
Empirey10 <genzyy04@gmail.com>
2023-02-21 22:00:12 -0800

    add file program

---
+++ Welcome4.java
@@ -0,0 +1,12 @@
+  // Fig. 2.4: Welcome4.Java
+  // Displaying multiple lines with method System.out.printf.
+
+  public class Welcome4
+  {
+    // main method begins execution of Java application
+    public static void main( String[] args )
+    {
+      System.out.printf( "%s\n%s\n",
+         "Welcome to", "Java Programming!" );
+    } // end method main
+ } // end class Welcome4
